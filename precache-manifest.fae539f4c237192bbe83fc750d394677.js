5de43b80b98a2e524c553e4259cc07c26b1c1b82
Serhii Zavadskyi <serhii.zavadskyi@gmail.com>
2020-05-25 13:21:28 +0300

    Updates

---
+++ precache-manifest.fae539f4c237192bbe83fc750d394677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e77241f7eb2ebc0b08ac002fc6419be4",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "27f19c7263ab0f1a93ce",
+    "url": "/pokedex/static/css/main.1d585028.chunk.css"
+  },
+  {
+    "revision": "9dea0897cad9735ef034",
+    "url": "/pokedex/static/js/2.b29e187b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokedex/static/js/2.b29e187b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f19c7263ab0f1a93ce",
+    "url": "/pokedex/static/js/main.8fac7943.chunk.js"
+  },
+  {
+    "revision": "3720d53d7f3565cf8d9c",
+    "url": "/pokedex/static/js/runtime-main.115cfb4e.js"
+  }
+]);
